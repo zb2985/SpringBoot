3a2ded437e749d45dc7b6780fa52e4b72a4c63da
zb2985 <zombie2985@naver.com>
2021-06-25 15:17:14 +0900

    프로젝트제출

--- src/main/java/kopo/jjh/prj/domain/entity/File.java
+++ src/main/java/kopo/jjh/prj/domain/entity/File.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -16,7 +13,7 @@ import javax.persistence.Id;
 public class File {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
